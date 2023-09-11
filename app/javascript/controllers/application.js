e64daecb030adc1e356b3408ccae5ae2e645443f
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-09-11 12:52:47 -0700

    Add quoets CRUD feature

---
+++ app/javascript/controllers/application.js
@@ -0,0 +1,11 @@
+import { Application } from "@hotwired/stimulus"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
+
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+export { application }
